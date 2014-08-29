4d132298ba2e18d44a22fdafa4f977d905617618
Fernando Pasik <fernando@pasik.com.ar>
2014-08-29 02:47:53 -0300

    App name in package.json and bower.json
    Template variable appname in package.json and bower.json, and tests.

--- test/specs/project.spec.js
+++ test/specs/project.spec.js
@@ -1,8 +1,12 @@
 'use strict';
 
-var assert = require('yeoman-generator').assert;
+var
+  helpers = require('yeoman-generator').test,
+  assert = require('yeoman-generator').assert;
 
 describe('Project Creation', function () {
+
+  var name = /\"name\": \"testapp\"/;
 
   it('creates expected files', function (done) {
     var expected = [
@@ -16,6 +20,28 @@ describe('Project Creation', function () {
       done();
     });
 
+  });
+
+  it('package.json filled template', function (done) {
+    helpers.mockPrompt(this.app, {
+      appName: 'testapp'
+    });
+
+    this.app.run({}, function () {
+      assert.fileContent('package.json', name);
+      done();
+    });
+  });
+
+  it('bower.json filled template', function (done) {
+    helpers.mockPrompt(this.app, {
+      appName: 'testapp'
+    });
+
+    this.app.run({}, function () {
+      assert.fileContent('bower.json', name);
+      done();
+    });
   });
 
 });
