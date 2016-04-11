2f687514ead20a5bd187e48961d9b5293565a943
Fernando Pasik <fernando@pasik.com.ar>
2016-04-11 00:17:26 -0300

    update author format in package.json and bower.json

--- test/project.spec.js
+++ test/project.spec.js
@@ -8,7 +8,6 @@ const
   assert = require('yeoman-assert'),
   name = /\"name\": \"testapp\"/,
   description = /\"description\": \"This is a test\"/,
-  git = {},
   expectedFiles = [
     'package.json',
     'bower.json',
@@ -18,7 +17,7 @@ const
 
 describe('Project Creation', () => {
 
-  let gen;
+  let gen, author;
 
   beforeEach(() => {
     gen = helpers
@@ -42,14 +41,14 @@ describe('Project Creation', () => {
   it('package.json filled template', done => {
     gen
       .on('ready', generator => {
-        git.user = new RegExp(`"name": "${generator.user.git.name()}"`);
-        git.email = new RegExp(`"email": "${generator.user.git.email()}"`);
+        author = new RegExp(
+          `"author": "${generator.user.git.name()} <${generator.user.git.email()}>"`
+        );
       })
       .on('end', () => {
         assert.fileContent('package.json', name);
         assert.fileContent('package.json', description);
-        assert.fileContent('package.json', git.user);
-        assert.fileContent('package.json', git.email);
+        assert.fileContent('package.json', author);
         done();
       });
   });
@@ -57,14 +56,14 @@ describe('Project Creation', () => {
   it('bower.json filled template', done => {
     gen
       .on('ready', generator => {
-        git.user = new RegExp(`"name": "${generator.user.git.name()}"`);
-        git.email = new RegExp(`"email": "${generator.user.git.email()}"`);
+        author = new RegExp(
+          `"author": "${generator.user.git.name()} <${generator.user.git.email()}>"`
+        );
       })
       .on('end', () => {
         assert.fileContent('bower.json', name);
         assert.fileContent('bower.json', description);
-        assert.fileContent('bower.json', git.user);
-        assert.fileContent('bower.json', git.email);
+        assert.fileContent('bower.json', author);
         done();
       });
   });
