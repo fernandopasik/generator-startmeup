91e8cc93c2199def2503d255c8bb4ba08db70490
Fernando Pasik <fernando@pasik.com.ar>
2016-04-14 23:14:19 -0300

    Extend base generator with recommended practices

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,11 +2,36 @@
 
 const generator = require('yeoman-generator');
 
+/**
+ * Generator definition
+ * @param {RunningMethods} All the methods that will be run with the priorities.
+ *
+ * The available priorities are (in order):
+ * 1. initializing - Your initialization methods
+ * 									 (checking current project state, getting configs, etc)
+ * 2. prompting    - Where you prompt users for options
+ * 								   (where you'd call this.prompt())
+ * 3. configuring  - Saving configurations and configure the project
+ *                   (creating .editorconfig files and other metadata files)
+ * 4. default      - If the method name doesn't match a priority,
+ *                   it will be pushed to this group.
+ * 5. writing      - Where you write the generator specific files
+ *                   (routes, controllers, etc)
+ * 6. conflicts    - Where conflicts are handled (used internally)
+ * 7. install      - Where installation are run (npm, bower)
+ * 8. end          - Called last, cleanup, say good bye, etc
+ */
 module.exports = generator.Base.extend({
   initializing: require('./inc/initializing'),
-  askForName: require('./inc/ask-for-name'),
-  lint: require('./inc/lint'),
-  project: require('./inc/project'),
-  dotfiles: require('./inc/dotfiles'),
+  prompting: {
+    askForName: require('./inc/ask-for-name'),
+    lint: require('./inc/lint')
+  },
+  configuring: {
+    dotfiles: require('./inc/dotfiles')
+  },
+  default: {
+    project: require('./inc/project')
+  },
   install: require('./inc/install')
 });
