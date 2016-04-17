e12a8f5a3dafe35a8d3cee84117bf6bc112ded0e
Fernando Pasik <fernando@pasik.com.ar>
2016-04-17 01:44:03 -0300

    Remove unnecessary tests

--- test/specs/project.spec.js
+++ test/specs/project.spec.js
@@ -4,8 +4,6 @@ const
   dirs = require('../helpers').dirs,
   yeomanTest = require('yeoman-test'),
   assert = require('yeoman-assert'),
-  name = /\"name\": \"testapp\"/,
-  description = /\"description\": \"This is a test\"/,
   expectedFiles = [
     'package.json',
     'bower.json',
@@ -15,7 +13,7 @@ const
 
 describe('Project Creation', () => {
 
-  let gen, author;
+  let gen;
 
   beforeEach(() => {
     gen = yeomanTest
@@ -34,36 +32,6 @@ describe('Project Creation', () => {
       assert.file(expectedFiles);
       done();
     });
-  });
-
-  it('package.json filled template', done => {
-    gen
-      .on('ready', generator => {
-        author = new RegExp(
-          `"author": "${generator.user.git.name()} <${generator.user.git.email()}>"`
-        );
-      })
-      .on('end', () => {
-        assert.fileContent('package.json', name);
-        assert.fileContent('package.json', description);
-        assert.fileContent('package.json', author);
-        done();
-      });
-  });
-
-  it('bower.json filled template', done => {
-    gen
-      .on('ready', generator => {
-        author = new RegExp(
-          `"authors": "${generator.user.git.name()} <${generator.user.git.email()}>"`
-        );
-      })
-      .on('end', () => {
-        assert.fileContent('bower.json', name);
-        assert.fileContent('bower.json', description);
-        assert.fileContent('bower.json', author);
-        done();
-      });
   });
 
 });
