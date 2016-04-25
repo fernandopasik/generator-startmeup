ff16cf6d8c3c7dd49737a0aa89530b0f449bd714
Fernando Pasik <fernando@pasik.com.ar>
2016-04-25 03:37:38 -0300

    Better initial bower.json

--- generators/app/writing.js
+++ generators/app/writing.js
@@ -5,9 +5,26 @@
  */
 module.exports = function () {
 
+  let bower;
+
   this.fs.writeJSON(this.destinationPath('package.json'), this.pkg);
+
   if (this.pkg.devDependencies.bower) {
+
     this.template('_bower.json', 'bower.json');
+
+    bower = Object.assign(
+      this.fs.readJSON(this.templatePath('_bower.json'), {}),
+      {
+        name: this.pkg.name,
+        description: this.pkg.description,
+        homepage: this.pkg.homepage,
+        authors: this.pkg.author,
+        repository: { type: 'git', url: this.pkg.repository.url }
+      }
+    );
+
+    this.fs.writeJSON(this.destinationPath('bower.json'), bower);
   }
   this.template('README.md');
   this.copy('LICENSE');
