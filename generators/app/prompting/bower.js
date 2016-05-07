9daf25ad03def05c262ea1839ca7fa435a0712ab
Fernando Pasik <fernando@pasik.com.ar>
2016-05-07 15:44:50 -0300

    Deprecate bower.json template

--- generators/app/prompting/bower.js
+++ generators/app/prompting/bower.js
@@ -16,6 +16,18 @@ module.exports = function () {
   ]).then(props => {
 
     if (props.bowerConfirm) {
+
+      this.bower = Object.assign(
+        this.fs.readJSON(this.templatePath('_bower.json'), {}),
+        {
+          name: this.pkg.name,
+          description: this.pkg.description,
+          homepage: this.pkg.homepage,
+          authors: this.pkg.author,
+          repository: { type: 'git', url: this.pkg.repository.url }
+        }
+      );
+
       Object.assign(this.pkg.devDependencies, { bower: 'latest' });
       this.dotfiles.push('.bowerrc');
     }
--- generators/app/writing.js
+++ generators/app/writing.js
@@ -4,28 +4,8 @@
  * Copy project initial files.
  */
 module.exports = function () {
-
-  let bower;
-
   this.fs.writeJSON(this.destinationPath('package.json'), this.pkg);
-
-  if (this.pkg.devDependencies.bower) {
-
-    this.template('_bower.json', 'bower.json');
-
-    bower = Object.assign(
-      this.fs.readJSON(this.templatePath('_bower.json'), {}),
-      {
-        name: this.pkg.name,
-        description: this.pkg.description,
-        homepage: this.pkg.homepage,
-        authors: this.pkg.author,
-        repository: { type: 'git', url: this.pkg.repository.url }
-      }
-    );
-
-    this.fs.writeJSON(this.destinationPath('bower.json'), bower);
-  }
+  this.fs.writeJSON(this.destinationPath('bower.json'), this.bower);
   this.template('README.md');
   this.copy('LICENSE');
 };
