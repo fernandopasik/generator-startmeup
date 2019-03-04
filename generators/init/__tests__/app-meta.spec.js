a7497ce87d134941dde4dae1afd1ce72f1009682
Fernando Pasik <fernando@pasik.com.ar>
2019-03-04 00:28:58 +0000

    chore: add jest

---
+++ generators/init/__tests__/app-meta.spec.js
@@ -0,0 +1,37 @@
+const appMeta = require('../app-meta');
+
+describe('App meta prompt', () => {
+  it('calls for prompt', async () => {
+    const prompt = jest.fn();
+
+    await appMeta.call({ prompt });
+
+    expect(prompt).toHaveBeenCalledWith(expect.any(Array));
+  });
+
+  it('asks for app name', async () => {
+    const prompt = jest.fn();
+
+    await appMeta.call({ prompt });
+
+    expect(prompt).toHaveBeenCalledWith(expect.arrayContaining([{
+      type: 'input',
+      name: 'name',
+      message: 'What is your app\'s name?',
+      default: 'generator-startmeup',
+    }]));
+  });
+
+  it('asks for app description', async () => {
+    const prompt = jest.fn();
+
+    await appMeta.call({ prompt });
+
+    expect(prompt).toHaveBeenCalledWith(expect.arrayContaining([{
+      type: 'input',
+      name: 'description',
+      message: 'What is your app\'s description?',
+      default: '',
+    }]));
+  });
+});
--- generators/init/app-meta.js
+++ generators/init/app-meta.js
@@ -5,13 +5,13 @@ module.exports = async function appMeta() {
     {
       type: 'input',
       name: 'name',
-      message: 'What is your app\'s name ?',
+      message: 'What is your app\'s name?',
       default: path.basename(process.cwd()),
     },
     {
       type: 'input',
       name: 'description',
-      message: 'What is your app\'s description ?',
+      message: 'What is your app\'s description?',
       default: '',
     },
   ]);
