a14d1ab4a1ae519c3ef1f881e59b255489e3fffb
Fernando Pasik <fernando@pasik.com.ar>
2019-03-07 17:51:44 +0000

    refactor: move to base prompt fields

--- generators/base.js
+++ generators/base.js
@@ -1,4 +1,5 @@
 const Generator = require('yeoman-generator');
+const path = require('path');
 
 module.exports = class Base extends Generator {
   constructor(args, opts) {
@@ -11,5 +12,66 @@ module.exports = class Base extends Generator {
   willInstall(dependency) {
     const { devDependencies = {} } = this.pkgJson;
     return devDependencies[dependency] || this.devDeps.includes(dependency);
+  }
+
+  async promptFields(fieldNames) {
+    const availableFields = {
+      appName: {
+        type: 'input',
+        name: 'appName',
+        message: 'What is your app\'s name?',
+        default: path.basename(process.cwd()),
+      },
+      appDescription: {
+        type: 'input',
+        name: 'appDescription',
+        message: 'What is your app\'s description?',
+        default: '',
+      },
+      authorName: {
+        type: 'input',
+        name: 'authorName',
+        message: 'What is your name?',
+        default: this.user.git.name(),
+      },
+      authorEmail: {
+        type: 'input',
+        name: 'authorEmail',
+        message: 'What is your email?',
+        default: this.user.git.email(),
+        when: props => props.authorName,
+      },
+      authorUrl: {
+        type: 'input',
+        name: 'authorUrl',
+        message: 'What is your url?',
+        default: '',
+        when: props => props.authorName,
+      },
+      githubConfirm: {
+        type: 'confirm',
+        name: 'githubConfirm',
+        default: true,
+        message: 'Are you going to use github?',
+      },
+      githubUsername: {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your github username?',
+        default: await this.user.github.username(),
+        when: props => props.githubConfirm,
+      },
+      githubRepo: {
+        type: 'input',
+        name: 'githubRepo',
+        message: 'What is your github repo?',
+        default: props => `https://github.com/${props.githubUsername}/${props.appName}`,
+        when: props => props.githubConfirm,
+      },
+    };
+
+    const selectedFields = fieldNames || Object.keys(availableFields);
+
+    this.answers = await this.prompt(selectedFields.map(fieldName => availableFields[fieldName]));
   }
 };
--- generators/init/index.js
+++ generators/init/index.js
@@ -1,26 +1,38 @@
 const path = require('path');
-const Generator = require('yeoman-generator');
-const getApp = require('./app-meta');
-const getAuthor = require('./author');
-const getGithub = require('./github');
 
-module.exports = class extends Generator {
+const Base = require('../base');
+
+module.exports = class extends Base {
   async prompting() {
-    const app = await getApp.call(this);
-    const author = await getAuthor.call(this);
-    const github = await getGithub.call(this, app.name);
-    const answers = {
-      app,
-      author,
-      github,
+    await this.promptFields();
+
+    const {
+      authorName,
+      authorEmail,
+      authorUrl,
+      githubRepo,
+    } = this.answers;
+
+    const options = {
+      ...this.answers,
+      author: [
+        authorName,
+        !authorEmail ? '' : ` <${authorEmail}>`,
+        !authorUrl ? '' : ` (${authorUrl})`,
+      ].join(''),
+      github: githubRepo ? {
+        homepage: githubRepo,
+        bugs: `${githubRepo}/issues`,
+        url: `${githubRepo}.git`,
+      } : {},
       year: new Date().getFullYear(),
     };
 
-    ['package.json', 'LICENSE', 'README.md'].forEach((template) => {
+    ['package.json'].forEach((template) => {
       this.fs.copyTpl(
         this.templatePath(template),
         this.destinationPath(template),
-        answers,
+        options,
       );
     });
 
