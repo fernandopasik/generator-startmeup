6f371851ac0df35d940cc1114255258d15b8ac1b
Fernando Pasik <fernando@pasik.com.ar>
2019-05-23 23:24:39 +0100

    refactor: info asking

--- generators/app/info.js
+++ generators/app/info.js
@@ -58,25 +58,43 @@ const questions = {
   },
 };
 
-const ask = async (questionNames = []) => {
-  const nonResponded = questionNames
-    .filter(questionName => questions[questionName])
-    .filter(questionName => !answers[questionName]);
+const filterKnownQuestions = questionNames => (
+  questionNames.filter(questionName => questions[questionName])
+);
 
-  const questionsToAsk = nonResponded.map(questionName => ({
+const filterRespondedQuestions = questionNames => (
+  questionNames.filter(questionName => typeof answers[questionName] === 'undefined')
+);
+
+const filterAnswers = (answersToFilter, questionNames) => (
+  Object.entries(answersToFilter)
+    .filter(([questionName]) => questionNames.includes(questionName))
+    .reduce((knownAnswers, [questionName, answer]) => (
+      Object.assign(knownAnswers, { [questionName]: answer })
+    ), {})
+);
+
+const ask = async (questionNames = []) => {
+  const knownQuestions = filterKnownQuestions(questionNames);
+  const questionsToAsk = filterRespondedQuestions(knownQuestions);
+
+  const toAsk = questionsToAsk.map(questionName => ({
     ...questions[questionName],
     name: questionName,
   }));
 
-  const responses = await api.prompt(questionsToAsk);
+  const responses = await api.prompt(toAsk);
 
-  nonResponded.forEach((questionName) => {
-    answers[questionName] = responses[questionName];
-  });
+  Object.assign(answers, responses);
+
+  return filterAnswers(answers, knownQuestions);
 };
 
 module.exports = {
-  setApi,
-  ask,
   answers,
+  ask,
+  filterAnswers,
+  filterKnownQuestions,
+  filterRespondedQuestions,
+  setApi,
 };
