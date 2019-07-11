3a58034ae977f826b33ad3fbac3c3e7451c8dbe4
Fernando Pasik <fernando@pasik.com.ar>
2019-07-11 23:41:25 +0100

    refactor: deprecate compiler code from libraries

--- generators/libraries/index.js
+++ generators/libraries/index.js
@@ -1,29 +1,19 @@
 const Generator = require('yeoman-generator');
 
 const dependencies = require('../app/dependencies');
-const util = require('../app/util');
+const compilerUtils = require('../compiler/compiler');
 const configReact = require('./react');
 const setupSrc = require('./setup-src');
-const setupTypescript = require('./setup-typescript');
 
 module.exports = class extends Generator {
+  initializing() {
+    const pkg = this.fs.readJSON('package.json');
+    this.existingCompiler = compilerUtils.detect(pkg);
+  }
+
   async prompting() {
     this.answers = {
       ...await this.prompt([
-        {
-          type: 'list',
-          name: 'compiler',
-          message: 'Which compiler do you want to use?',
-          choices: ['none', 'babel', 'typescript'],
-          default: 1,
-        },
-        {
-          type: 'confirm',
-          name: 'flow',
-          message: 'Do you want to use flow static type checking?',
-          default: false,
-          when: props => props.compiler === 'babel',
-        },
         {
           type: 'list',
           name: 'uiLibrary',
@@ -36,49 +26,20 @@ module.exports = class extends Generator {
   }
 
   configuring() {
-    const { compiler, flow, uiLibrary } = this.answers;
-
-    if (compiler === 'babel') {
-      this.babelConfig = {
-        presets: ['@babel/preset-env'],
-      };
-
-      dependencies.addDev(['@babel/cli', '@babel/core', '@babel/preset-env']);
-
-      if (flow) {
-        dependencies.addDev(['@babel/preset-flow', 'flow-bin']);
-        this.babelConfig.presets.push('@babel/preset-flow');
-      }
-    }
-
-    if (compiler === 'typescript') {
-      dependencies.addDev(['typescript']);
-    }
+    const { uiLibrary } = this.answers;
 
     if (uiLibrary === 'lit-html') {
       dependencies.add(['lit-html', 'lit-element']);
     }
 
     if (uiLibrary === 'react') {
-      configReact.call(this, this.answers.compiler);
+      configReact.call(this, this.existingCompiler);
     }
   }
 
   writing() {
-    const { compiler, uiLibrary } = this.answers;
-
-    if (compiler === 'babel') {
-      this.fs.write(
-        this.destinationPath('babel.config.js'),
-        util.formatJsConfig(this.babelConfig),
-      );
-    }
-
-    if (compiler === 'typescript') {
-      setupTypescript.call(this);
-    }
-
-    setupSrc.call(this, compiler === 'typescript', uiLibrary === 'react');
+    const { uiLibrary } = this.answers;
+    setupSrc.call(this, this.existingCompiler === 'typescript', uiLibrary === 'react');
   }
 
   install() {
