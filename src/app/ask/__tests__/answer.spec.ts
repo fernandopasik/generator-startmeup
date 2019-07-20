ca3f42aa8baea67a3f02be2ee2b2b6f393b9740c
Fernando Pasik <fernando@pasik.com.ar>
2019-07-20 02:35:33 +0100

    feat: memory in answers

--- src/app/ask/__tests__/answer.spec.ts
+++ src/app/ask/__tests__/answer.spec.ts
@@ -1,6 +1,286 @@
-import { flatten } from '../answer';
+import {
+  flatten,
+  forgetAll,
+  get,
+  getAll,
+  has,
+  remember,
+  rememberAll,
+  forget,
+  areUnanswered,
+} from '../answer';
 
 describe('Answer', () => {
+  describe('remember', () => {
+    beforeEach(() => {
+      forgetAll();
+    });
+
+    it('saves in memory an answer', () => {
+      const answer = 'this is an example';
+      remember('example', answer);
+
+      expect(get('example')).toBe(answer);
+    });
+
+    it('replaces existing stored answer', () => {
+      const answer = 'this is an example';
+      const anotherAnswer = 'this is another example';
+      remember('example', answer);
+      remember('example', anotherAnswer);
+
+      expect(get('example')).toBe(anotherAnswer);
+    });
+  });
+
+  describe('rememberAll', () => {
+    beforeEach(() => {
+      forgetAll();
+    });
+
+    it('saves in memory a group of answers', () => {
+      const answers = {
+        answer1: 'value1',
+        answer2: 'value2',
+        answer3: 'value3',
+      };
+      rememberAll(answers);
+
+      expect(getAll()).toStrictEqual(answers);
+    });
+
+    it('saves in existing memory a group of answers', () => {
+      const answers1 = {
+        answer1: 'value1',
+        answer2: 'value2',
+      };
+      const answers2 = {
+        answer2: 'value2',
+        answer3: 'value3',
+      };
+      rememberAll(answers1);
+      rememberAll(answers2);
+
+      expect(getAll()).toStrictEqual({
+        ...answers1,
+        ...answers2,
+      });
+    });
+
+    it('handles empty group of answers', () => {
+      rememberAll({});
+
+      expect(getAll()).toStrictEqual({});
+    });
+  });
+
+  describe('get', () => {
+    beforeEach(() => {
+      forgetAll();
+    });
+
+    it('can get an answer', () => {
+      const answers = {
+        answer1: 'value1',
+        answer2: 'value2',
+      };
+      rememberAll(answers);
+
+      expect(get('answer1')).toBe('value1');
+    });
+
+    it('can get a group of answers', () => {
+      const answers = {
+        answer1: 'value1',
+        answer2: 'value2',
+        answer3: 'value3',
+      };
+      rememberAll(answers);
+
+      expect(get(['answer1', 'answer2'])).toStrictEqual({ answer1: 'value1', answer2: 'value2' });
+    });
+
+    it('can handle non existent answer', () => {
+      const answers = {
+        answer1: 'value1',
+        answer2: 'value2',
+      };
+      rememberAll(answers);
+
+      expect(get('answer3')).toBeUndefined();
+    });
+  });
+
+  describe('getAll', () => {
+    beforeEach(() => {
+      forgetAll();
+    });
+
+    it('can get all answers', () => {
+      const answers = {
+        answer1: 'value1',
+        answer2: 'value2',
+      };
+      rememberAll(answers);
+
+      expect(getAll()).toStrictEqual(answers);
+    });
+
+    it('can get a group of answers', () => {
+      const answers = {
+        answer1: 'value1',
+        answer2: 'value2',
+        answer3: 'value3',
+      };
+      rememberAll(answers);
+
+      expect(getAll(['answer1', 'answer2'])).toStrictEqual({
+        answer1: 'value1',
+        answer2: 'value2',
+      });
+    });
+
+    it('can get a group of some known answers', () => {
+      const answers = {
+        answer1: 'value1',
+        answer2: 'value2',
+        answer3: 'value3',
+      };
+      rememberAll(answers);
+
+      expect(getAll(['answer1', 'answer2', 'answer4'])).toStrictEqual({
+        answer1: 'value1',
+        answer2: 'value2',
+      });
+    });
+  });
+
+  describe('has', () => {
+    beforeEach(() => {
+      forgetAll();
+    });
+
+    it('with existing answer', () => {
+      const answers = {
+        answer1: 'value1',
+        answer2: 'value2',
+      };
+      rememberAll(answers);
+
+      expect(has('answer1')).toBe(true);
+    });
+
+    it('with non existing answer', () => {
+      const answers = {
+        answer1: 'value1',
+        answer2: 'value2',
+      };
+      rememberAll(answers);
+
+      expect(has('answer3')).toBe(false);
+    });
+  });
+
+  describe('forget', () => {
+    beforeEach(() => {
+      forgetAll();
+    });
+
+    it('an existing answer', () => {
+      const answers = {
+        answer1: 'value1',
+        answer2: 'value2',
+      };
+      rememberAll(answers);
+
+      forget('answer1');
+
+      expect(get('answer1')).toBeUndefined();
+      expect(getAll()).toStrictEqual({ answer2: 'value2' });
+    });
+
+    it('an non existing answer', () => {
+      const answers = {
+        answer1: 'value1',
+        answer2: 'value2',
+      };
+      rememberAll(answers);
+
+      forget('answer3');
+
+      expect(getAll()).toStrictEqual(answers);
+    });
+
+    it('when no answers are remembered', () => {
+      forget('answer3');
+
+      expect(getAll()).toStrictEqual({});
+    });
+  });
+
+  describe('forget', () => {
+    beforeEach(() => {
+      forgetAll();
+    });
+
+    it('when existing answers', () => {
+      const answers = {
+        answer1: 'value1',
+        answer2: 'value2',
+      };
+      rememberAll(answers);
+
+      forgetAll();
+
+      expect(getAll()).toStrictEqual({});
+    });
+
+    it('when non existing answers', () => {
+      forgetAll();
+
+      expect(getAll()).toStrictEqual({});
+    });
+  });
+
+  describe('areUnanswered', () => {
+    beforeEach(() => {
+      forgetAll();
+    });
+
+    it('when some are', () => {
+      const answers = {
+        answer1: 'value1',
+        answer2: 'value2',
+      };
+      rememberAll(answers);
+
+      expect(areUnanswered(['answer2', 'answer3', 'answer4'])).toStrictEqual([
+        'answer3',
+        'answer4',
+      ]);
+    });
+
+    it('when all are', () => {
+      const answers = {
+        answer1: 'value1',
+        answer2: 'value2',
+      };
+      rememberAll(answers);
+
+      expect(areUnanswered(['answer3', 'answer4'])).toStrictEqual(['answer3', 'answer4']);
+    });
+
+    it('when none are', () => {
+      const answers = {
+        answer1: 'value1',
+        answer2: 'value2',
+      };
+      rememberAll(answers);
+
+      expect(areUnanswered(['answer1', 'answer2'])).toStrictEqual([]);
+    });
+  });
+
   describe('flatten', () => {
     it('handles empty answers', () => {
       expect({}).toStrictEqual({});
--- src/app/ask/answer.ts
+++ src/app/ask/answer.ts
@@ -1,5 +1,42 @@
 import { Answers } from 'inquirer';
 
+const memory: Map<string, any> = new Map();
+
+export const remember = (answerName: string, answer: any): void => {
+  memory.set(answerName, answer);
+};
+
+export const rememberAll = (answers: Answers): void => {
+  Object.keys(answers).forEach(answerName => {
+    remember(answerName, answers[answerName]);
+  });
+};
+
+export const getAll = (answerNames?: string[]): Answers => {
+  const obj: Answers = {};
+  for (let [key, value] of memory) {
+    if (!answerNames || answerNames.includes(key)) {
+      obj[key] = value;
+    }
+  }
+  return obj;
+};
+
+export const get = (answerNames?: string | string[]): any => {
+  if (typeof answerNames === 'string') {
+    return memory.get(answerNames);
+  }
+
+  return getAll(answerNames);
+};
+
+export const has = (answerName: string): boolean => memory.has(answerName);
+export const forget = (answerName: string): boolean => memory.delete(answerName);
+export const forgetAll = (): void => memory.clear();
+
+export const areUnanswered = (questionNames: string[]) =>
+  questionNames.filter(questionName => !memory.has(questionName));
+
 export const flatten = (answers: Answers, namePrefix: string = ''): Answers =>
   Object.keys(answers).reduce((flattened: Answers, answerName: string) => {
     const name = namePrefix ? `${namePrefix}.${answerName}` : answerName;
