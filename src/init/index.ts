caa9e70e6cc0f1b6685651bc09057d9c70253988
Fernando Pasik <fernando@pasik.com.ar>
2020-03-01 15:12:21 +0000

    fix: move init files to templates

--- src/init/index.ts
+++ src/init/index.ts
@@ -1,14 +1,11 @@
 import Generator from 'yeoman-generator';
-import path from 'path';
 
 export default class InitGenerator extends Generator {
   public writing(): void {
-    const getSrcPath = (filename: string): string => path.join(__dirname, '../../', filename);
-
-    this.fs.copy(getSrcPath('.editorconfig'), this.destinationPath('.editorconfig'));
-    this.fs.copy(getSrcPath('.gitattributes'), this.destinationPath('.gitattributes'));
-    if (!this.fs.exists(getSrcPath('.gitignore'))) {
-      this.fs.copy(getSrcPath('.gitignore'), this.destinationPath('.gitignore'));
+    this.fs.copy(this.templatePath('editorconfig'), this.destinationPath('.editorconfig'));
+    this.fs.copy(this.templatePath('gitattributes'), this.destinationPath('.gitattributes'));
+    if (!this.fs.exists(this.destinationPath('.gitignore'))) {
+      this.fs.copy(this.templatePath('gitignore'), this.destinationPath('.gitignore'));
     }
   }
 }
