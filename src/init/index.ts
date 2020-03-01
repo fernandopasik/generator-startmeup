d9ece2bf240416d013a9d662a08d039c19c33f55
Fernando Pasik <fernando@pasik.com.ar>
2020-03-01 14:41:52 +0000

    fix: remove npmignore copy from init subgenerator

--- src/init/index.ts
+++ src/init/index.ts
@@ -3,10 +3,8 @@ import path from 'path';
 
 export default class InitGenerator extends Generator {
   public writing(): void {
-    ['.editorconfig', '.gitattributes', '.gitignore', '.npmignore'].forEach(
-      (file: string): void => {
-        this.fs.copy(path.join(__dirname, '../../', file), this.destinationPath(file));
-      },
-    );
+    ['.editorconfig', '.gitattributes', '.gitignore'].forEach((file: string): void => {
+      this.fs.copy(path.join(__dirname, '../../', file), this.destinationPath(file));
+    });
   }
 }
