0c08d4a3b71d786d00c96d1627c08f9e5d9216ae
Fernando Pasik <fernando@pasik.com.ar>
2020-03-01 15:25:57 +0000

    refactor: move github files into templates

--- src/docs/index.ts
+++ src/docs/index.ts
@@ -1,5 +1,4 @@
 import Generator from 'yeoman-generator';
-import path from 'path';
 import { Answers } from 'inquirer';
 
 import ask from '../app/ask';
@@ -40,19 +39,18 @@ module.exports = class extends Generator {
       year: new Date().getFullYear(),
     };
 
-    ['LICENSE', 'README.md', 'CONTRIBUTING.md'].forEach((template: string): void => {
-      this.fs.copyTpl(this.templatePath(template), this.destinationPath(template), options);
-    });
+    ['LICENSE', 'README.md', 'CONTRIBUTING.md', 'CODE_OF_CONDUCT.md'].forEach(
+      (template: string): void => {
+        this.fs.copyTpl(this.templatePath(template), this.destinationPath(template), options);
+      },
+    );
 
-    const files = [
-      '.github/PULL_REQUEST_TEMPLATE.md',
-      '.github/ISSUE_TEMPLATE/bug_report.md',
-      '.github/ISSUE_TEMPLATE/feature_request.md',
-      'CODE_OF_CONDUCT.md',
-    ];
-
-    files.forEach((file: string): void => {
-      this.fs.copy(path.join(__dirname, '../../', file), this.destinationPath(file));
+    [
+      'github/PULL_REQUEST_TEMPLATE.md',
+      'github/ISSUE_TEMPLATE/bug_report.md',
+      'github/ISSUE_TEMPLATE/feature_request.md',
+    ].forEach((template: string): void => {
+      this.fs.copyTpl(this.templatePath(template), this.destinationPath(`.${template}`), options);
     });
   }
 };
