415d6f253d77790fe4bcb63471d54f57cc95b557
Fernando Pasik <fernando@pasik.com.ar>
2020-03-01 14:55:55 +0000

    fix: do not override existing gitignore

--- src/init/index.ts
+++ src/init/index.ts
@@ -3,8 +3,12 @@ import path from 'path';
 
 export default class InitGenerator extends Generator {
   public writing(): void {
-    ['.editorconfig', '.gitattributes', '.gitignore'].forEach((file: string): void => {
-      this.fs.copy(path.join(__dirname, '../../', file), this.destinationPath(file));
-    });
+    const getSrcPath = (filename: string): string => path.join(__dirname, '../../', filename);
+
+    this.fs.copy(getSrcPath('.editorconfig'), this.destinationPath('.editorconfig'));
+    this.fs.copy(getSrcPath('.gitattributes'), this.destinationPath('.gitattributes'));
+    if (!this.fs.exists(getSrcPath('.gitignore'))) {
+      this.fs.copy(getSrcPath('.gitignore'), this.destinationPath('.gitignore'));
+    }
   }
 }
