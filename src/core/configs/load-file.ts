0fc0ab74efc4cb3857026005752922b9ddee930a
Fernando Pasik <fernando@pasik.com.ar>
2020-03-23 01:59:57 +0000

    fix: set imported file in cache with filename key

--- src/core/configs/load-file.ts
+++ src/core/configs/load-file.ts
@@ -4,7 +4,7 @@ import store, { Config } from './store';
 const loadFile = async <T = Config>(filename: string): Promise<T> => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   const { default: config } = (await import(currentPath(filename))) as { default: T };
-  store.set('filename', config);
+  store.set(filename, config);
   return config;
 };
 
