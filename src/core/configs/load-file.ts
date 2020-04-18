23a8f60623c2a9e14b52eb32fe133c4b755cb57b
Fernando Pasik <fernando@pasik.com.ar>
2020-04-18 12:01:43 +0100

    style: remove eslint ignore due to fixed rule

--- src/core/configs/load-file.ts
+++ src/core/configs/load-file.ts
@@ -2,7 +2,6 @@ import currentPath from './current-path';
 import store, { Config } from './store';
 
 const loadFile = async <T = Config>(filename: string): Promise<T> => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   const { default: config } = (await import(currentPath(filename))) as { default: T };
   store.set(filename, config);
   return config;
