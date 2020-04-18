e657a020d6882bd208eeccef090ac3d5e7cce7c1
Fernando Pasik <fernando@pasik.com.ar>
2020-04-18 23:18:03 +0100

    refactor: split format method

--- src/core/configs/__tests__/format.spec.ts
+++ src/core/configs/__tests__/format.spec.ts
@@ -1,11 +1,27 @@
 import prettier from 'prettier';
 import { mocked } from 'ts-jest/utils';
-import format from '../format';
+import format, { stringifyJson } from '../format';
 import loadPrettierConfig from '../load-prettier-config';
 
 jest.mock('../load-prettier-config');
 
 describe('format', () => {
+  it('stringifies json without spaces if has more than one property', () => {
+    const myConfig = { test1: true, test2: true };
+
+    expect(stringifyJson(myConfig)).toMatchInlineSnapshot(`"{\\"test1\\":true,\\"test2\\":true}"`);
+  });
+
+  it('stringifies json with spaces if has one property', () => {
+    const myConfig = { test1: true };
+
+    expect(stringifyJson(myConfig)).toMatchInlineSnapshot(`
+      "{
+        \\"test1\\": true
+      }"
+    `);
+  });
+
   it('calls prettier', async () => {
     const myConfig = { test: true };
     const spy = jest.spyOn(prettier, 'format');
