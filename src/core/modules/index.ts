f1c866fd912ee689384dc18d3f09b21d26cbbd2f
Fernando Pasik <fernando@pasik.com.ar>
2020-04-19 15:09:17 +0100

    feat: run module method

--- src/core/modules/index.ts
+++ src/core/modules/index.ts
@@ -1,3 +1,4 @@
 export { default as confirm } from './confirm';
 export { default as isPresent } from './is-present';
 export { default as load } from './load';
+export { default as run } from './run';
---
+++ src/core/modules/run.ts
@@ -0,0 +1,26 @@
+import { add as addDependencies } from '../dependencies';
+import { set as setConfig } from '../configs';
+import confirm from './confirm';
+import store, { moduleMainDependency } from './store';
+
+const run = async (moduleName: string): Promise<void> => {
+  const config = store.get(moduleName);
+
+  if (typeof config === 'undefined') {
+    return;
+  }
+
+  if (config.confirm ?? false) {
+    const confirmed = await confirm(moduleName, config?.confirmMessage);
+
+    if (confirmed) {
+      config.mainDependencies.forEach(({ name, type }: Readonly<moduleMainDependency>) => {
+        addDependencies(name, type);
+      });
+
+      setConfig(config.configFilename, config.configContent);
+    }
+  }
+};
+
+export default run;
