e7b96a2965b5c31fc544bccd09e9d3e3b1319bc0
Fernando Pasik <fernando@pasik.com.ar>
2020-04-19 19:12:10 +0100

    refactor: deprecate unused questions in prettier module

--- src/prettier/config.ts
+++ src/prettier/config.ts
@@ -1,5 +1,3 @@
-import { modules } from '../core';
-
 export default {
   configFilename: '.prettierrc.json',
   configContent: {
@@ -16,14 +14,6 @@ export default {
     {
       name: 'prettier',
       type: 'devDependencies',
-    },
-  ],
-  questions: [
-    {
-      type: 'confirm',
-      name: 'prettier',
-      default: (): boolean => modules.isPresent('prettier'),
-      message: 'Do you want to use prettier to format files?',
     },
   ],
   replaceConfig: true,
