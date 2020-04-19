f44dc2d0810be267bfe6c0d386ea1991587bd060
Fernando Pasik <fernando@pasik.com.ar>
2020-04-19 15:07:08 +0100

    feat: have a default confirm message if none present in module config

--- src/core/modules/confirm.ts
+++ src/core/modules/confirm.ts
@@ -1,7 +1,9 @@
 import ask from '../ask';
 import isPresent from './is-present';
 
-const confirm = async (moduleName: string, message: string): Promise<boolean> => {
+const confirm = async (moduleName: string, confirmMessage?: string): Promise<boolean> => {
+  const message = confirmMessage ?? `Do you want to use ${moduleName}?`;
+
   const response = (await ask([
     {
       type: 'confirm',
