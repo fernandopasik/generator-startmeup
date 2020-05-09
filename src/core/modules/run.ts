42f306779cc1a51c2476e08519ad5a05fcf2627a
Fernando Pasik <fernando@pasik.com.ar>
2020-05-09 04:11:48 +0100

    fix: if not confirmed return to stop it

--- src/core/modules/run.ts
+++ src/core/modules/run.ts
@@ -13,17 +13,19 @@ const run = async (moduleName: string): Promise<void> => {
   if (config.confirm ?? false) {
     const confirmed = await confirm(moduleName, config?.confirmMessage);
 
-    if (confirmed) {
-      config.mainDependencies.forEach(({ name, type }: Readonly<moduleMainDependency>) => {
-        addDependencies(name, type);
-      });
-
-      setConfig(
-        config.configFilename,
-        typeof config.configContent === 'function' ? config.configContent() : config.configContent,
-      );
+    if (!confirmed) {
+      return;
     }
   }
+
+  config.mainDependencies.forEach(({ name, type }: Readonly<moduleMainDependency>) => {
+    addDependencies(name, type);
+  });
+
+  setConfig(
+    config.configFilename,
+    typeof config.configContent === 'function' ? config.configContent() : config.configContent,
+  );
 };
 
 export default run;
