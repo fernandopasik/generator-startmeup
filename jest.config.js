352dcca0a1df1f1d780962595bea84f9978c8b11
Fernando Pasik <fernando@pasik.com.ar>
2020-06-11 23:37:43 +0100

    test: add missing tsconfig in jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   collectCoverageFrom: ['src/**/*.ts'],
+  globals: { 'ts-jest': { tsConfig: 'tsconfig.all.json' } },
   testEnvironment: 'node',
   transform: { '^.+\\.[j|t]s$': 'ts-jest' },
 };
