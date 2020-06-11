34950c8218c8234ea20b7d24eb377c6ad1297247
Fernando Pasik <fernando@pasik.com.ar>
2020-06-11 23:32:52 +0100

    feat: set package json options for main files

--- src/src/src.ts
+++ src/src/src.ts
@@ -8,20 +8,44 @@ export default class SrcGenerator extends Generator {
   }
 
   public async writing(): Promise<void> {
-    const pkg = await configs.load<PackageJson>('package.json');
+    const pkg = (await configs.load('package.json')) as PackageJson;
+    const { main, name } = pkg;
 
-    if (typeof pkg?.main === 'undefined' || !this.fs.exists(this.destinationPath(pkg.main))) {
-      let extension = 'js';
-
-      if (dependencies.has('typescript', 'dev')) {
-        extension = 'ts';
-      }
-
-      if (dependencies.has('react')) {
-        extension += 'x';
-      }
-
-      this.fs.write(this.destinationPath(`src/index.${extension}`), '');
+    if (this.fs.exists(this.destinationPath(main as string))) {
+      return;
     }
+
+    let extension = 'js';
+
+    if (dependencies.has('typescript', 'dev')) {
+      extension = 'ts';
+    }
+
+    if (dependencies.has('react')) {
+      extension += 'x';
+    }
+
+    const mainBuiltFile = `${name}.js`;
+    const builtFiles = `/${name}.*`;
+
+    const packageProps: Record<string, boolean | string | string[]> = {
+      sideEffects: false,
+      type: 'module',
+      main: mainBuiltFile,
+      module: mainBuiltFile,
+      files: [builtFiles],
+    };
+
+    if (dependencies.has('typescript', 'dev')) {
+      packageProps.typings = `${name}.d.ts`;
+    }
+
+    configs.set('package.json', {
+      ...pkg,
+      ...packageProps,
+    });
+
+    const mainFile = `src/${name}.${extension}`;
+    this.fs.write(this.destinationPath(mainFile), '');
   }
 }
