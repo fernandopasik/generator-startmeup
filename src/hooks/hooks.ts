f46ca8f85e96e0732e2b777f3440a75e15024351
Fernando Pasik <fernando@pasik.com.ar>
2021-04-22 19:41:23 +0100

    feat: update hooks generator to new husky

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,27 +1,35 @@
-import type { modules } from '../core';
-import { Generator } from '../core';
-import config from './config';
+import Generator from 'yeoman-generator';
+import { dependencies } from '../core';
 
 export default class HooksGenerator extends Generator {
-  public moduleConfig: modules.ModuleConfig = config;
-
   public async initializing(): Promise<void> {
-    await super.initializing();
+    await dependencies.importAll();
   }
 
-  public async prompting(): Promise<void> {
-    await super.prompting();
+  public configuring(): void {
+    dependencies.add('husky', 'dev');
+    dependencies.add('pinst', 'dev');
   }
 
-  public async configuring(): Promise<void> {
-    await super.configuring();
-  }
+  public writing(): void {
+    if (dependencies.has('@commitlint/cli', 'dev')) {
+      this.copyHookConfig('commit-msg');
+    }
 
-  public async writing(): Promise<void> {
-    await super.writing();
+    if (dependencies.has('lint-staged', 'dev')) {
+      this.copyHookConfig('pre-commit');
+    }
+
+    this.copyHookConfig('pre-push');
   }
 
   public install(): void {
-    super.install();
+    if (!(this.options['skip-install'] as boolean)) {
+      dependencies.install();
+    }
+  }
+
+  private copyHookConfig(name: string): void {
+    this.fs.copyTpl(this.templatePath(name), this.destinationPath(`.husky/${name}`));
   }
 }
