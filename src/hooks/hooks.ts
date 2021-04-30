4a2b78bc1ec4b7196a7176eff7b065c799aad9d5
Fernando Pasik <fernando@pasik.com.ar>
2021-04-30 23:45:28 +0100

    feat: use yeoman native package management

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,19 +1,25 @@
 import type { PackageJson } from 'type-fest';
 import Generator from 'yeoman-generator';
-import { dependencies } from '../core';
 
 export default class HooksGenerator extends Generator {
-  public async initializing(): Promise<void> {
-    await dependencies.importAll();
-  }
-
   public configuring(): void {
-    dependencies.add('husky', 'dev');
-    dependencies.add('pinst', 'dev');
-  }
+    // @ts-expect-error not yet in types
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    this.packageJson.merge({
+      scripts: {
+        postinstall: 'husky install',
+        prepublishOnly: 'pinst --disable',
+        postpublish: 'pinst --enable',
+      },
+    });
 
-  public writing(): void {
-    const { devDependencies = {} } = this.readDestinationJSON('package.json') as PackageJson;
+    // @ts-expect-error not yet in types
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    this.addDevDependencies({ husky: '^6.0.0', pinst: '^2.1.6' });
+
+    // @ts-expect-error not yet in types
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    const devDependencies = this.packageJson.get('devDependencies') as PackageJson;
 
     if ('@commitlint/cli' in devDependencies) {
       this.copyTemplate('commit-msg', '.husky/commit-msg');
@@ -24,11 +30,5 @@ export default class HooksGenerator extends Generator {
     }
 
     this.copyTemplate('pre-push', '.husky/pre-push');
-  }
-
-  public install(): void {
-    if (!(this.options['skip-install'] as boolean)) {
-      dependencies.install();
-    }
   }
 }
