cdd1222f9adf028c014b271fdd19a2771ceafce0
Fernando Pasik <fernando@pasik.com.ar>
2021-05-15 18:38:18 +0100

    refactor: migrate generator to native yeoman methods

--- src/lint-staged/lint-staged.ts
+++ src/lint-staged/lint-staged.ts
@@ -1,27 +1,73 @@
-import type { modules } from '../core';
-import { Generator } from '../core';
-import config from './config';
+import type { PackageJson } from 'type-fest';
+import Generator from 'yeoman-generator';
+import { format } from '../core/configs';
+
+interface Answers {
+  confirm: boolean;
+}
 
 export default class LintStagedGenerator extends Generator {
-  public moduleConfig: modules.ModuleConfig = config;
-
-  public async initializing(): Promise<void> {
-    await super.initializing();
-  }
+  public answers: Answers = { confirm: true };
 
   public async prompting(): Promise<void> {
-    await super.prompting();
+    this.answers = await this.prompt<Answers>([
+      {
+        type: 'confirm',
+        name: 'confirm',
+        message: 'Do you want to use lint-staged for pre-commit hook?',
+        default: true,
+      },
+    ]);
   }
 
   public async configuring(): Promise<void> {
-    await super.configuring();
-  }
+    if (!this.answers.confirm) {
+      return;
+    }
 
-  public async writing(): Promise<void> {
-    await super.writing();
-  }
+    // @ts-expect-error not yet in types
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    this.addDevDependencies({ 'lint-staged': '^11.0.0' });
 
-  public install(): void {
-    super.install();
+    // @ts-expect-error not yet in types
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    const devDependencies = this.packageJson.get('devDependencies') as PackageJson;
+    // @ts-expect-error not yet in types
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    const dependencies = this.packageJson.get('dependencies') as PackageJson;
+
+    const config: Record<string, string[] | string> = {};
+    const jsExtensions = ['js'];
+
+    if ('react' in dependencies || 'react' in devDependencies) {
+      jsExtensions.push('jsx');
+    }
+
+    if ('typescript' in devDependencies) {
+      jsExtensions.push('ts');
+
+      if ('react' in dependencies || 'react' in devDependencies) {
+        jsExtensions.push('tsx');
+      }
+    }
+
+    const jsCommands = [];
+
+    if ('eslint' in devDependencies) {
+      jsCommands.push('eslint');
+    }
+
+    if ('jest' in devDependencies) {
+      jsCommands.push('jest --bail --findRelatedTests');
+    }
+
+    if (jsCommands.length > 0) {
+      const matcher = `*.{${jsExtensions.join(',')}}`;
+      config[matcher] = jsCommands;
+    }
+
+    const formattedConfig = await format(config);
+
+    this.writeDestination('.lintstagedrc.json', formattedConfig);
   }
 }
