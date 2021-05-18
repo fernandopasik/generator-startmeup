28e8b4d37c4f625ff1e30350fb2bc4228a708c6d
Fernando Pasik <fernando@pasik.com.ar>
2021-05-18 19:39:53 +0100

    refactor: migrate lint generator

--- src/lint/lint.ts
+++ src/lint/lint.ts
@@ -1,27 +1,75 @@
-import type { modules } from '../core';
-import { Generator } from '../core';
-import config from './config';
+import Generator from '../generator';
 
 export default class LintGenerator extends Generator {
-  public moduleConfig: modules.ModuleConfig = config;
-
-  public async initializing(): Promise<void> {
-    await super.initializing();
-  }
-
-  public async prompting(): Promise<void> {
-    await super.prompting();
-  }
-
   public async configuring(): Promise<void> {
-    await super.configuring();
+    const extensions = this.getJsExtensions();
+
+    this.packageJson.merge({
+      scripts: {
+        lint: `eslint . --ext ${extensions.join(',')}`,
+      },
+    });
+
+    await this.addDevDependencies(['eslint']);
   }
 
   public async writing(): Promise<void> {
-    await super.writing();
-  }
+    const options = {
+      babel: this.hasDevDependency('@babel/core'),
+      jest: this.hasDevDependency('jest'),
+      lit: this.hasAnyDependency('lit') || this.hasAnyDependency('lit-html'),
+      prettier: this.hasDevDependency('prettier'),
+      puppeteer: this.hasDevDependency('puppeteer'),
+      react: this.hasAnyDependency('react'),
+      storybook: this.hasDevDependency('storybook'),
+      typescript: this.hasDevDependency('typescript'),
+    };
 
-  public install(): void {
-    super.install();
+    if (options.react) {
+      await this.addDevDependencies([
+        'eslint-config-airbnb',
+        'eslint-plugin-import',
+        'eslint-plugin-jsx-a11y',
+        'eslint-plugin-react',
+      ]);
+    } else {
+      await this.addDevDependencies(['eslint-config-airbnb-base', 'eslint-plugin-import']);
+    }
+
+    if (options.typescript) {
+      await this.addDevDependencies([
+        '@typescript-eslint/eslint-plugin',
+        '@typescript-eslint/parser',
+      ]);
+    }
+
+    if (options.babel) {
+      await this.addDevDependencies(['babel-eslint']);
+    }
+
+    if (options.lit) {
+      await this.addDevDependencies([
+        'eslint-plugin-lit',
+        'eslint-plugin-lit-a11y',
+        'eslint-plugin-wc',
+      ]);
+    }
+
+    if (options.jest) {
+      await this.addDevDependencies(['eslint-plugin-jest']);
+    }
+
+    if (options.prettier) {
+      await this.addDevDependencies(['eslint-config-prettier', 'eslint-plugin-prettier']);
+    }
+
+    this.renderTemplate('eslintrc.json', '.eslintrc.json', options, { rmWhitespace: true });
+
+    const config = this.readDestination('.eslintrc.json');
+
+    const configJSON = config.replace(/[\s]*\/\//g, '');
+    const formattedConfig = await this.formatFile(configJSON);
+
+    this.writeDestination('.eslintrc.json', formattedConfig);
   }
 }
