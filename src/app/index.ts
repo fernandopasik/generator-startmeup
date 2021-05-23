a416415df0d9d1879c98aa075cdf982d29c099c1
Fernando Pasik <fernando@pasik.com.ar>
2021-05-23 21:21:40 +0100

    feat: run generators with optional confirmation

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,9 +1,15 @@
-import Generator from 'yeoman-generator';
 import yosay from 'yosay';
 import { dependencies } from '../core';
+import Generator from '../generator';
+
+interface Confirmation {
+  message: string;
+  default: boolean;
+  skip: boolean;
+}
 
 export default class StartMeUpGenerator extends Generator {
-  public initializing(): void {
+  public async initializing(): Promise<void> {
     this.log(yosay('Welcome to the marvelous StartMeUp generator!'));
 
     const subGenerators = [
@@ -13,7 +19,7 @@ export default class StartMeUpGenerator extends Generator {
       'prettier',
       'commit-lint',
       'libraries',
-      'compilers',
+      'typescript',
       'jest',
       'lint',
       'lint-staged',
@@ -22,9 +28,29 @@ export default class StartMeUpGenerator extends Generator {
       'src',
     ];
 
-    subGenerators.forEach((subGenerator: string): void => {
-      this.composeWith(`startmeup:${subGenerator}`, { 'skip-install': true });
-    });
+    const subGeneratorConfirmations: Record<string, Confirmation> = {
+      typescript: {
+        message: 'Are you programming with TypeScript?',
+        default: true,
+        skip: this.hasFiles('**/*.ts'),
+      },
+    };
+
+    await Promise.all(
+      subGenerators.map(async (subGenerator: string): Promise<void> => {
+        const confirmation = subGeneratorConfirmations[subGenerator];
+
+        let run = true;
+
+        if (typeof confirmation !== 'undefined' && !confirmation.skip) {
+          run = await this.confirm(confirmation.message, confirmation.default);
+        }
+
+        if (run) {
+          this.composeWith(`startmeup:${subGenerator}`, { 'skip-install': true });
+        }
+      }),
+    );
   }
 
   public install(): void {
