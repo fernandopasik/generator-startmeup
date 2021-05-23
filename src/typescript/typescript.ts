7491e33b97b05a3919476876d6e331f56a9b98aa
Fernando Pasik <fernando@pasik.com.ar>
2021-05-23 12:24:43 +0100

    feat: typescript generator

---
+++ src/typescript/typescript.ts
@@ -0,0 +1,44 @@
+import type { PackageJson } from 'type-fest';
+import Generator from '../generator';
+
+export default class TypescriptGenerator extends Generator {
+  public async configuring(): Promise<void> {
+    this.packageJson.merge({
+      scripts: {
+        'check-types': 'tsc --noEmit -p tsconfig.all.json',
+        build: 'tsc',
+      },
+    });
+
+    await this.addDevDependencies(['typescript']);
+  }
+
+  public async writing(): Promise<void> {
+    const packageFiles = (this.packageJson.get('files') as PackageJson['files']) ?? [];
+    const exclude = [...packageFiles];
+
+    if (this.hasDevDependency('flow-bin')) {
+      exclude.push('**/*.flow');
+    }
+
+    const options = {
+      excludeFiles: exclude.map((file) => `"${file}"`).join(','),
+      flow: this.hasDevDependency('flow-bin'),
+      jest: this.hasDevDependency('jest'),
+      jestEnzyme: this.hasDevDependency('jest-enzyme'),
+      module: this.packageJson.get('type') === 'module' ? 'ESNext' : 'commonjs',
+      outDir: packageFiles.includes('/lib') ? '.' : packageFiles[0].replace(/^\//g, ''),
+      puppeteer: this.hasDevDependency('puppeteer'),
+      react: this.hasAnyDependency('react'),
+      storybook:
+        this.hasDevDependency('@storybook/react') ||
+        this.hasDevDependency('@storybook/web-components'),
+    };
+
+    this.renderTemplate('tsconfig.json', 'tsconfig.json', options, { rmWhitespace: true });
+    this.renderTemplate('tsconfig.all.json', 'tsconfig.all.json', options, { rmWhitespace: true });
+
+    await this.formatFile('tsconfig.json');
+    await this.formatFile('tsconfig.all.json');
+  }
+}
