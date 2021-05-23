03f02684408ed92148fd72a467b8bb27d24eecc3
Fernando Pasik <fernando@pasik.com.ar>
2021-05-23 22:13:50 +0100

    refactor: libraries generator

--- src/libraries/libraries.ts
+++ src/libraries/libraries.ts
@@ -1,56 +1,27 @@
-import Generator from 'yeoman-generator';
-import { ask, dependencies } from '../core';
+import Generator from '../generator';
 
-const LIBRARIES = ['lit-html', 'react'];
+const FRAMEWORKS = ['none', 'lit', 'react'];
 
 export default class LibrariesGenerator extends Generator {
-  private libraries: string[] = [];
-
-  public async initializing(): Promise<void> {
-    await dependencies.importAll();
-  }
-
   public async prompting(): Promise<void> {
-    const { libraries } = await ask([
+    const { framework } = await this.prompt<{ framework: string }>([
       {
-        type: 'checkbox',
-        name: 'libraries',
-        message: 'Which UI library do you want to use?',
-        choices: LIBRARIES,
-        default: LIBRARIES.filter((library: string): boolean => dependencies.has(library, 'all')),
+        type: 'list',
+        name: 'framework',
+        message: 'Which framework do you want to use?',
+        choices: FRAMEWORKS,
+        default: FRAMEWORKS.find((frameworkName) => this.hasAnyDependency(frameworkName)),
       },
     ]);
 
-    this.libraries = libraries as string[];
-
-    if (this.libraries.includes('lit-html')) {
-      dependencies.add('lit-html', 'peer');
-      dependencies.add('lit-element', 'peer');
+    if (framework === 'lit') {
+      await this.addDevDependencies(['lit']);
+      await this.addPeerDependencies(['lit']);
     }
 
-    if (this.libraries.includes('react')) {
-      dependencies.add('react', 'peer');
-      dependencies.add('react-dom', 'peer');
-      dependencies.add('react-test-renderer', 'dev');
-    }
-  }
-
-  public configuring(): void {
-    if (this.libraries.includes('react')) {
-      if (dependencies.has('@babel/core', 'dev')) {
-        dependencies.add('@babel/preset-react', 'dev');
-      }
-
-      if (dependencies.has('typescript', 'dev')) {
-        dependencies.add('@types/react', 'dev');
-        dependencies.add('@types/react-dom', 'dev');
-      }
-    }
-  }
-
-  public install(): void {
-    if (!(this.options['skip-install'] as boolean)) {
-      dependencies.install();
+    if (framework === 'react') {
+      await this.addDevDependencies(['react', 'react-dom', 'react-test-renderer']);
+      await this.addPeerDependencies(['react', 'react-dom']);
     }
   }
 }
