06fc6f3efe60cb7be0be36e25e003159c90a0305
Fernando Pasik <fernando@pasik.com.ar>
2021-06-03 22:06:15 +0100

    refactor: render and format json config

--- src/commit-lint/commit-lint.ts
+++ src/commit-lint/commit-lint.ts
@@ -4,6 +4,6 @@ export default class CommitLintGenerator extends Generator {
   public async configuring(): Promise<void> {
     await this.addDevDependencies(['@commitlint/cli', '@commitlint/config-conventional']);
 
-    this.copyTemplate('commitlintrc.json', '.commitlintrc.json');
+    await this.renderTpl('commitlintrc.json', '.commitlintrc.json');
   }
 }
