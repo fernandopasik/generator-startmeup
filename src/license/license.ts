5e930264d8be4daccfbbd81402bfb0bc692e8b30
Fernando Pasik <fernando@pasik.com.ar>
2021-06-15 00:20:19 +0100

    refactor: read first commit for license year

--- src/license/license.ts
+++ src/license/license.ts
@@ -1,16 +1,23 @@
 import Generator from '../generator';
 import { parseAuthor } from '../packagejson';
+import firstCommit from '../utils/first-commit';
 
 export default class LicenseGenerator extends Generator {
   public configuring(): void {
     const author = this.packageJson.get('author') as string;
     const { name: authorName } = parseAuthor(author);
 
-    const options = {
-      authorName,
-      year: this.getFirstCommitYear(),
-    };
+    const commit = firstCommit();
 
-    this.renderTemplate('LICENSE', 'LICENSE', options);
+    if (commit !== null) {
+      const year = new Date(commit.date).getFullYear();
+
+      const options = {
+        authorName,
+        year,
+      };
+
+      this.renderTemplate('LICENSE', 'LICENSE', options);
+    }
   }
 }
