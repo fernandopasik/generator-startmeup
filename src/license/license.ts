3278b9987ebb0c5eb842fc4dfad860dbd57835fc
Fernando Pasik <fernando@pasik.com.ar>
2021-06-16 00:21:27 +0100

    refactor: use first commit author for license

--- src/license/license.ts
+++ src/license/license.ts
@@ -1,19 +1,15 @@
 import Generator from '../generator';
-import { parseAuthor } from '../packagejson';
 import firstCommit from '../utils/first-commit';
 
 export default class LicenseGenerator extends Generator {
   public configuring(): void {
-    const author = this.packageJson.get('author') as string;
-    const { name: authorName } = parseAuthor(author);
-
     const commit = firstCommit();
 
     if (commit !== null) {
       const year = new Date(commit.date).getFullYear();
 
       const options = {
-        authorName,
+        authorName: commit.author,
         year,
       };
 
