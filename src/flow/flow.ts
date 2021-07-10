fcaa0e036069d33fee3f229e31f4722ee8a47476
Fernando Pasik <fernando@pasik.com.ar>
2021-07-10 02:44:40 +0100

    feat: flow generator

---
+++ src/flow/flow.ts
@@ -0,0 +1,26 @@
+import type { PackageJson } from 'type-fest';
+import Generator from '../generator';
+
+export default class FlowGenerator extends Generator {
+  public async configuring(): Promise<void> {
+    const name = this.packageJson.get('name') as PackageJson['name'];
+
+    if (typeof name === 'undefined') {
+      return;
+    }
+
+    this.packageJson.merge({
+      scripts: {
+        flowgen: `flowgen ${name}.d.ts --quiet -o ${name}.js.flow`,
+      },
+    });
+
+    await this.addDevDependencies(['flow-bin', 'flowgen']);
+  }
+
+  public async writing(): Promise<void> {
+    if (this.hasDevDependency('flow-bin')) {
+      await this.renderTpl('flowconfig', '.flowconfig');
+    }
+  }
+}
