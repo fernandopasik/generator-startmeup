61cf89cb9cb0e189b56fa2115e469f736774537c
Fernando Pasik <fernando@pasik.com.ar>
2021-10-10 10:57:49 +0100

    fix: use single extension in lint staged

--- src/lint-staged/lint-staged.ts
+++ src/lint-staged/lint-staged.ts
@@ -6,11 +6,19 @@ export default class LintStagedGenerator extends Generator {
   }
 
   public async writing(): Promise<void> {
+    const getExtensionsTarget = (extensions: readonly string[]): string | undefined =>
+      // eslint-disable-next-line no-nested-ternary
+      extensions.length !== 0
+        ? extensions.length > 1
+          ? `*.{${extensions.join(',')}}`
+          : `*.${extensions[0]}`
+        : undefined;
+
     const options = {
-      cssExtensions: this.getExtensions('css').join(','),
+      cssTarget: getExtensionsTarget(this.getExtensions('css')),
       eslint: this.hasDevDependency('eslint'),
       jest: this.hasDevDependency('jest'),
-      jsExtensions: this.getExtensions().join(','),
+      jsTarget: getExtensionsTarget(this.getExtensions()),
       prettier: this.hasDevDependency('prettier'),
       scss: this.hasFiles('**/*.scss'),
       stylelint: this.hasDevDependency('stylelint'),
