f39a8a7fb522e72b49ba82d50fe28a665af353a6
Fernando Pasik <fernando@pasik.com.ar>
2021-10-16 02:16:00 +0100

    test: ignore templates in coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  collectCoverageFrom: ['src/**/*.{j,t}s'],
+  collectCoverageFrom: ['src/**/*.{j,t}s', '!src/**/templates/**'],
   globals: { 'ts-jest': { tsconfig: 'tsconfig.all.json', useESM: true } },
   moduleNameMapper: { '(.*)\\.js': '$1' },
   preset: 'ts-jest/presets/js-with-ts-esm',
