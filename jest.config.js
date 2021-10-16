d4172b2ae52702e08d380feba8803f7d0810f85e
Fernando Pasik <fernando@pasik.com.ar>
2021-10-16 02:50:56 +0100

    build: switch to tsconfig build

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   collectCoverageFrom: ['src/**/*.{j,t}s', '!src/**/templates/**'],
-  globals: { 'ts-jest': { tsconfig: 'tsconfig.all.json', useESM: true } },
+  globals: { 'ts-jest': { useESM: true } },
   moduleNameMapper: { '(.*)\\.js': '$1' },
   preset: 'ts-jest/presets/js-with-ts-esm',
   testEnvironment: 'node',
