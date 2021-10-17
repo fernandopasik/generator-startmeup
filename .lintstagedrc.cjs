46727fcc82145ef09a829817005cbaf39eca25e4
Fernando Pasik <fernando@pasik.com.ar>
2021-10-17 16:19:48 +0100

    build: check types in lint staged

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,4 +1,23 @@
+const fs = require('fs');
+
+const lintTSConfig = 'tsconfig.lint.json';
+
+const generateTSConfig = (stagedFilenames) => {
+  const tsconfig = JSON.parse(fs.readFileSync('tsconfig.json', 'utf8'));
+  tsconfig.include = stagedFilenames;
+  fs.writeFileSync(lintTSConfig, JSON.stringify(tsconfig));
+  return 'tsc --noEmit --project tsconfig.lint.json';
+};
+
+const deleteTSConfig = (stagedFilenames) => {
+  stagedFilenames.length = 0;
+  if (fs.existsSync(lintTSConfig)) {
+    fs.unlinkSync(lintTSConfig);
+  }
+  return '';
+};
+
 module.exports = {
   '*': ['prettier --check'],
-  '*.{js,ts}': ['eslint', 'jest --bail --findRelatedTests'],
+  '*.{js,ts}': ['eslint', generateTSConfig, deleteTSConfig, 'jest --bail --findRelatedTests'],
 };
