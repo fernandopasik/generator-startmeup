4860addc7dc90ff922159ba37fec7715c60a37a7
Fernando Pasik <fernando@pasik.com.ar>
2021-10-17 16:10:34 +0100

    fix: delete temporary tsconfig only if exists

--- src/lint-staged/templates/lintstagedrc.cjs
+++ src/lint-staged/templates/lintstagedrc.cjs
@@ -1,15 +1,21 @@
 // <% if (typescript) { %>
 const fs = require('fs');
 
+const lintTSConfig = 'tsconfig.lint.json';
+
 const generateTSConfig = (stagedFilenames) => {
   const tsconfig = JSON.parse(fs.readFileSync('tsconfig.json', 'utf8'));
   tsconfig.include = stagedFilenames;
-  fs.writeFileSync('tsconfig.lint.json', JSON.stringify(tsconfig));
+  fs.writeFileSync(lintTSConfig, JSON.stringify(tsconfig));
   return 'tsc --noEmit --project tsconfig.lint.json';
 };
 
-const deleteTSConfig = () => {
-  fs.unlink('tsconfig.lint.json');
+const deleteTSConfig = (stagedFilenames) => {
+  stagedFilenames.length = 0;
+  if (fs.existsSync(lintTSConfig)) {
+    fs.unlinkSync(lintTSConfig);
+  }
+  return '';
 };
 // <% } %>
 
