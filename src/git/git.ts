317193fe4e70592d941dab17de37f22d80673295
Fernando Pasik <fernando@pasik.com.ar>
2023-04-18 16:25:12 +0100

    feat: load the rest of gitignore and render it in template

--- src/git/git.ts
+++ src/git/git.ts
@@ -3,10 +3,15 @@ import Generator from '../generator.js';
 export default class GitGenerator extends Generator {
   public writing(): void {
     this.renderTemplate('gitattributes', '.gitattributes');
+    const existingGitIgnore = this.fs.read('.gitignore');
+    const restGitIgnore = existingGitIgnore
+      .replace(/[\S\s]*# build and temp folders\n/m, '')
+      .trim();
 
     const options = {
       package: this.isNpmPackage(),
       packageManager: this.getPackageManager(),
+      restGitIgnore,
     };
 
     this.renderTemplate('gitignore', '.gitignore', options);
