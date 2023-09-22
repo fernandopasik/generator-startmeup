e1f1f9c446c90bd05b6b42772def7b33421ac1c7
Fernando Pasik <fernando@pasik.com.ar>
2023-09-22 17:40:38 +0100

    test: mock individually prettier methods

--- src/utils/__tests__/prettier-format.spec.ts
+++ src/utils/__tests__/prettier-format.spec.ts
@@ -1,41 +1,43 @@
-import prettier from 'prettier';
+import { format, resolveConfig } from 'prettier';
 import prettierFormat from '../prettier-format.js';
 
+jest.mock('prettier', () => ({
+  format: jest.fn(),
+  resolveConfig: jest.fn(),
+}));
+
 describe('prettierFormat', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
   describe('resolves config', () => {
     it('with the provided root path', async () => {
-      const spy = jest.spyOn(prettier, 'resolveConfig');
       const root = '/my-test';
       await prettierFormat('', 'example.js', root);
-      expect(spy).toHaveBeenCalledWith(root);
-      spy.mockRestore();
+      expect(resolveConfig).toHaveBeenCalledWith(root);
     });
 
     it('formats the file content', async () => {
-      const spy = jest.spyOn(prettier, 'format');
       const content = '{ "foo": "bar" }';
       await prettierFormat(content, 'example.json', '/');
-      expect(spy).toHaveBeenCalledWith(content, expect.anything());
-      spy.mockRestore();
+      expect(format).toHaveBeenCalledWith(content, expect.anything());
     });
   });
 
   describe('sets in options', () => {
     it('the filepath', async () => {
-      const spy = jest.spyOn(prettier, 'format');
       const filepath = 'example.json';
       await prettierFormat('', filepath, '/');
-      expect(spy).toHaveBeenCalledWith('', expect.objectContaining({ filepath }));
-      spy.mockRestore();
+      expect(format).toHaveBeenCalledWith('', expect.objectContaining({ filepath }));
     });
 
     it('the resolved config', async () => {
-      const spy1 = jest.spyOn(prettier, 'resolveConfig').mockResolvedValue({ printWidth: 130 });
-      const spy2 = jest.spyOn(prettier, 'format');
+      (resolveConfig as jest.MockedFunction<typeof resolveConfig>).mockResolvedValueOnce({
+        printWidth: 130,
+      });
       await prettierFormat('', 'example.js', '/');
-      expect(spy2).toHaveBeenCalledWith('', expect.objectContaining({ printWidth: 130 }));
-      spy1.mockRestore();
-      spy2.mockRestore();
+      expect(format).toHaveBeenCalledWith('', expect.objectContaining({ printWidth: 130 }));
     });
   });
 });
