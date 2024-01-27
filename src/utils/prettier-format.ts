cd835567934d4c732e5662544391f538a5400c97
Fernando Pasik <fernando@pasik.com.ar>
2024-01-27 16:08:48 +0000

    fix: temporary fix for running prettier on tsconfig files

--- src/utils/prettier-format.ts
+++ src/utils/prettier-format.ts
@@ -3,7 +3,11 @@ import { format, resolveConfig, resolveConfigFile } from 'prettier';
 
 const prettierFormat = async (content: string, filepath: string, root: string): Promise<string> => {
   const configFile = await resolveConfigFile(path.join(root, filepath));
-  const config = configFile ? await resolveConfig(configFile) : {};
+  const config = configFile ? (await resolveConfig(configFile)) ?? {} : {};
+
+  if (filepath.includes('tsconfig.')) {
+    config.parser = 'json';
+  }
 
   return format(content, { ...config, filepath });
 };
