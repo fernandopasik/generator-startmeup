b2879e524ecce82a34b6055c5847c7be3bfd5fac
Fernando Pasik <fernando@pasik.com.ar>
2024-01-27 12:48:55 +0000

    refactor: join report to main workflow when private repo

--- src/github-actions/github-actions.ts
+++ src/github-actions/github-actions.ts
@@ -8,11 +8,13 @@ export default class GithubActionsGenerator extends Generator {
     }
 
     const name = (this.packageJson.get('name') as PackageJson['name']) ?? '';
+    const isPrivate = (this.packageJson.get('private') as PackageJson['private']) ?? false;
     const scripts = (this.packageJson.get('scripts') as PackageJson['scripts']) ?? {};
 
     const options = {
       yarn: this.hasFiles('./yarn.lock') ? (this.hasFiles('./.yarn') ? 2 : 1) : 0,
       checkFormat: 'format:check' in scripts,
+      isPrivate,
       lint: 'lint' in scripts,
       checkTypes: 'check-types' in scripts,
       unitTests: 'test:coverage' in scripts,
@@ -28,8 +30,12 @@ export default class GithubActionsGenerator extends Generator {
 
     await this.renderTpl('main.yml', '.github/workflows/main.yml', options);
 
-    if (options.lint || options.unitTests || options.e2eTests) {
+    if (!isPrivate && (options.lint || options.unitTests || options.e2eTests)) {
       await this.renderTpl('reports.yml', '.github/workflows/reports.yml', options);
+    }
+
+    if (isPrivate) {
+      this.fs.delete(['.github/workflows/reports.yml']);
     }
   }
 }
