938141ea51b1b4a3edc15e955a15af49369db865
Fernando Pasik <fernando@pasik.com.ar>
2024-06-21 19:50:12 +0100

    refactor: prettier config load

--- src/utils/prettier-format.ts
+++ src/utils/prettier-format.ts
@@ -1,9 +1,13 @@
 import path from 'node:path';
-import { format, resolveConfig, resolveConfigFile } from 'prettier';
+import { format, resolveConfig, resolveConfigFile, type Options } from 'prettier';
 
 const prettierFormat = async (content: string, filepath: string, root: string): Promise<string> => {
   const configFile = await resolveConfigFile(path.join(root, filepath));
-  const config = configFile ? (await resolveConfig(configFile)) ?? {} : {};
+  let config: Options = {};
+
+  if (configFile !== null) {
+    config = (await resolveConfig(configFile)) ?? {};
+  }
 
   if (filepath.includes('tsconfig.')) {
     config.parser = 'json';
